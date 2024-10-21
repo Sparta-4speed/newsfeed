409cc1617f2630aab9f74029df5fa45507a1ab8f
eun0 <shinely12@gmail.com>
2024-10-21 14:22:39 +0900

    init commit - CRUD

---
+++ src/main/java/com/sparta/newsfeed/entity/Timestamped.java
@@ -0,0 +1,24 @@
+package com.sparta.newsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
